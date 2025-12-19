b6db62eb747ebaddfc9f79feeb3ee058c3bdbfea
Merlin Dumeur <merlin@dumeur.net>
2025-12-19 13:08:19 +0200

    Unbiased jax estimation

--- pymultifracs/jax.py
+++ pymultifracs/jax.py
@@ -73,7 +73,7 @@ def _binom(N, k):
     return jnp.exp(gammaln(N + 1) - gammaln(k + 1) - gammaln(N - k + 1))
 
 
-def _compute_Cmj(pleader_p, j, p, ZPJCorr, validity, n_cumul):
+def _compute_Cmj(pleader_p, j, p, ZPJCorr, validity, n_cumul, bias=True):
 
     T_X_j = jnp.log(pleader_p ** (1/p) * ZPJCorr)
 
@@ -92,21 +92,33 @@ def _compute_Cmj(pleader_p, j, p, ZPJCorr, validity, n_cumul):
             aux = aux + _binom(m-1, n-1) * Cmj[ind_n] * Mu[ind_m-ind_n-1]
 
         Cmj = Cmj.at[ind_m].set(Mu[ind_m] - aux)
+    
+    if not bias:
+
+        N_useful = validity.sum(axis=0)
+        correction_factor = jnp.ones_like(N_useful, dtype=float)
+
+        for m in range(2, n_cumul+1):
+
+            ind_m = m-1
+            correction_factor = correction_factor * N_useful / (N_useful - (m-1)) #* 10
+            Cmj = Cmj.at[ind_m].set(Cmj[ind_m] * correction_factor)
 
     return Cmj
 
 
-def _compute_Cm_jax(pleader_p, j1, j2, n_cumul, p, ZPJCorr, validity):
+def _compute_Cm_jax(pleader_p, j1, j2, n_cumul, p, ZPJCorr, validity, bias=True):
 
     Cm = jnp.stack(
         [
             _compute_Cmj(pleader_p[j], j, p, ZPJCorr[:, j-1], validity[j],
-                         n_cumul)
+                         n_cumul, bias)
             for ind_j, j in enumerate(range(j1, j2+1))
         ],
         axis=0
     )
 
+    # shape j, m, channel
     return Cm
 
 
