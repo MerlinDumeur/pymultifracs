4f4b64794384b85e6049cca249640278cd9d6a0f
Merlin Dumeur <merlin@dumeur.net>
2026-04-09 01:29:32 +0300

    Test updates, jax refresh

---
+++ pymultifracs/jax/__init__.py
@@ -0,0 +1,6 @@
+from .utils import _binom
+from .wavelet import integrate_wt, _compute_leaders_jax, _correct_pleaders_jax
+from .scalingfun import _compute_Cmj, _compute_Cm_jax
+from .estimation import compute_etap
+from .mfa import _compute_weights, _compute_cm_jax
+from .simul import _gaussian_cme, _skewness_conv
